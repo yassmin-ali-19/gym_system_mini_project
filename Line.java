475684ca877ba25dd10829a0072248063254c7c8
unknown <yassmingaber372@gmail.com>
2024-10-26 21:46:36 +0300

    1st file edited

--- Line.java
+++ Line.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package trainer;
 
-/**
- *
- * @author Yassmin
- */
-public class Line {
-    
+
+public abstract class Line {
+    abstract String lineRepresentation();
+    abstract String getSearchKey();
 }
