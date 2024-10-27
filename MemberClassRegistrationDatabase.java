69b4b5803fd128c7d7b1dfafca0f255d530e56b7
AhmedxTamer <medotamer200490@gmail.com>
2024-10-27 03:28:38 +0300

    test

---
+++ MemberClassRegistrationDatabase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg4.gym.management.system;
+
+/**
+ *
+ * @author C O N N E C T
+ */
+public class MemberClassRegistrationDatabase {
+//    public void readFromFile(){
+//        try {
+//            BufferedReader r = new BufferedReader(new FileReader(filename));
+//            
+//            String line;
+//            while((line = r.readLine()) != null){
+//                line.split(",");
+//            }
+//            
+//            
+//            r.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+}
